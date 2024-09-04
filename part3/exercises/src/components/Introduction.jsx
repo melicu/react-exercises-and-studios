b2603e54f611de29a83ac38b54a688fbce2b1675
unknown <melcusumano@gmail.com>
2024-09-03 23:15:28 -0500

    React Part 3 Exercises Complete

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Cross-Stitch</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Cross-stitch can be traced back to the Middle Ages.</li>
+            <li>Evenweave fabrics such as linen and aida are most common for cross-stitch.</li>
+            <li>Varying the number of threads used for a stitch greatly affects the final result.</li>
          </ol>
       </div>
    );
